8970e4583846dd8dd622ea01cb1dc622b7d42734
perkview <perkview.earn@gmail.com>
2025-12-18 14:32:03 +0500

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('products/', views.products_list, name='products'),
